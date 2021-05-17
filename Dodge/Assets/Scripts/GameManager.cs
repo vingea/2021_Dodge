942ee864b7c91f95e4b9600b5dd745282cdff610
vingea <84334744+vingea@users.noreply.github.com>
2021-05-18 00:02:33 +0900

    닷지 플레리러 1.0
    닷지 플레이어 1.0

---
+++ Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text Timetext;
+    public Text Recordtext;
+
+    public GameObject level;
+    public GameObject bulletSpawnerPrefeb;
+    private Vector3[] bulletSpawners = new Vector3[4];
+    int spawnercount= 0;
+
+    private float survivetime;
+    private bool isGameover;
+
+    void Start()
+    {
+        survivetime = 0;
+        isGameover = false;
+
+        bulletSpawners[0].x = -8f;
+        bulletSpawners[0].y = 1f;
+        bulletSpawners[0].z = 8f;
+
+        bulletSpawners[1].x = 8f;
+        bulletSpawners[1].y = 1f;
+        bulletSpawners[1].z = 8f;
+
+        bulletSpawners[2].x = 8f;
+        bulletSpawners[2].y = 1f;
+        bulletSpawners[2].z = -8f;
+
+        bulletSpawners[3].x = -8f;
+        bulletSpawners[3].y = 1f;
+        bulletSpawners[3].z = -8f;
+    }
+
+
+    void Update()
+    {
+        if(!isGameover)
+        {
+            survivetime += Time.deltaTime;
+            Timetext.text = "Time: " + (int)survivetime;
+
+            if(survivetime < 5f && spawnercount == 0)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefeb, bulletSpawners[3], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnercount];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnercount++;
+            }
+            else if(survivetime >= 5f && survivetime < 10f && spawnercount == 1)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefeb, bulletSpawners[3], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnercount];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnercount++;
+            }
+            else if (survivetime >= 10f && survivetime < 15f && spawnercount == 2)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefeb, bulletSpawners[3], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnercount];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnercount++;
+            }
+            else if (survivetime >= 15f && survivetime < 20f && spawnercount == 3)
+            {
+                GameObject bulletSpawner = Instantiate(bulletSpawnerPrefeb, bulletSpawners[3], Quaternion.identity);
+                bulletSpawner.transform.parent = level.transform;
+                bulletSpawner.transform.localPosition = bulletSpawners[spawnercount];
+                level.GetComponent<Rotator>().rotationSpeed += 15f;
+                spawnercount++;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if(survivetime > bestTime)
+        {
+            bestTime = survivetime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+        
+        Recordtext.text = "Best Time: " + (int) bestTime;
+    }
+
+    
+}
---
+++ Dodge/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 60f;
+
+    void Start()
+    {
+        transform.Rotate(0f, rotationSpeed, 0f);
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
